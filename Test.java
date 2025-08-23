60d5daf24424d2c7ebc4b7fabd5b0b30a98a5614
Suchith K <suchith004@gmail.com>
2025-08-23 19:42:36 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
         int b=200;
         int res=a+b;
         System.out.println("the result is " +res);
-        system.out.println("Welcome to Jenkins First Session"
+        System.out.println("Welcome to Jenkins First Session");
     }
     public static void main(String[] args)
     {
