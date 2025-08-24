bf055fe08c271b079677523ff6ed0424dca5ed58
Suchith K <suchith004@gmail.com>
2025-08-24 11:38:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,8 +5,8 @@ public class Test {
         int a=100;
         int b=200;
         int res=a+b;
-        System.out.println("the result is " +res);
-        System.out.println("Welcome to Jenkins First Session");
+        System.out.println("The result is " +res);
+        System.out.println("Welcome to Docker")
     }
     public static void main(String[] args)
     {
