8f71748b9ad5229fe4b223a022a13232d1077467
Suchith K <suchith004@gmail.com>
2025-08-29 20:41:52 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
         int b=200;
         int res=a+b;
         System.out.println("The result is " +res);
-        System.out.println("Welcome to Docker")
+        System.out.println("Welcome to Docker");
     }
     public static void main(String[] args)
     {
