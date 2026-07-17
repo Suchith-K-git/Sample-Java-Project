26c29fb23cf328bc3ba79f16582706709caf8236
Suchith K <suchith004@gmail.com>
2026-07-17 19:30:57 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,7 +8,7 @@ public class Test {
         System.out.println("The result is " +res);
         system.out.println("Welcome to Jenkins")
     }
-    public static void main(String[] args)
+
     {
         add()
     }
